07e7d199b700cdc341abda60d7c7586f04c7509b
miltonbanksy <miltonbanksy@gmail.com>
2025-08-26 01:34:40 +0900

    Made a start on the random tarot

--- tarot.js
+++ tarot.js
@@ -2,15 +2,62 @@ const tarot_cards = {
     "Ace of Cups":{
         "suit": "cups",
         "value": "1",
-        "element": "water",
+        "suit element": "water",
         "suit keywords": "emotion, feelings, habits, healing, intuition, relationships, spirituality",
-        "duality": "passive, receptive",
-        "astro": "cancer, scorpio, pisces",
-        "alt names": "chalices, goblets, vessels",
-        "jung": "feeling",
-        "direction": "west",
-        "time": "autumn, dusk",
-        "yes-no": "yes"
+        "suit duality": "passive, receptive",
+        "suit astrology": "cancer, scorpio, pisces",
+        "suit alt names": "chalices, goblets, vessels",
+        "suit jung": "feeling",
+        "suit direction": "west",
+        "suit time": "autumn, dusk",
+        "yes-no": "yes",
+        "symbols": [
+            "lotuses in water (enlightenment, rebirth, fertility)",
+            "dove (peace, love, divine inspiration)",
+            "five streams of water (four elements and aether)"
+        ],
+        "upright keywords": "love, new feelings, emotional awakening, creativity, spirituality, intuition, joy, happiness",
+        "reversed keywords": "coldness, emptiness, emotional loss, blocked creativity, feeling unloved, gloominess, sadness"
+    },
+    "Two of Cups":{
+        "suit": "cups",
+        "value": "2",
+        "suit element": "water",
+        "suit keywords": "emotion, feelings, habits, healing, intuition, relationships, spirituality",
+        "suit duality": "passive, receptive",
+        "suit astrology": "cancer, scorpio, pisces",
+        "suit alt names": "chalices, goblets, vessels",
+        "suit jung": "feeling",
+        "suit direction": "west",
+        "suit time": "autumn, dusk",
+        "yes-no": "yes",
+        "symbols": [
+            "lion (courage, justice)",
+            "caduceus (balance, healing, harmony)",
+            "two figures (equal footing, trust)"
+        ],
+        "upright keywords": "unity, partnership, attraction, connection, close bonds, mutual respect, emotional intimacy",
+        "reversed keywords": "separation, rejection, division, imbalance, tension, bad communication, withdrawal, distance"
+    },
+    "Three of Cups":{
+        "suit": "cups",
+        "value": "3",
+        "suit element": "water",
+        "suit keywords": "emotion, feelings, habits, healing, intuition, relationships, spirituality",
+        "suit duality": "passive, receptive",
+        "suit astrology": "cancer, scorpio, pisces",
+        "suit alt names": "chalices, goblets, vessels",
+        "suit jung": "feeling",
+        "suit direction": "west",
+        "suit time": "autumn, dusk",
+        "yes-no": "yes",
+        "symbols": [
+            "clear skies (easy times, wisdom)",
+            "fruits (abundance, joyful gatherings)",
+            "red robe (passion, vitality)"
+        ],
+        "upright keywords": "unity, partnership, attraction, connection, close bonds, mutual respect, emotional intimacy",
+        "reversed keywords": "separation, rejection, division, imbalance, tension, bad communication, withdrawal, distance"
     }
 }
 
