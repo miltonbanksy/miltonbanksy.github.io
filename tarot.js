17cfda73c0fc2c0f8a284d3a5c0c2c4ff7dc75d7
miltonbanksy <miltonbanksy@gmail.com>
2025-08-26 14:24:33 +0900

    updating tarot

--- tarot.js
+++ tarot.js
@@ -1,63 +1,101 @@
 const tarot_cards = {
-    "Ace of Cups":{
-        "suit": "cups",
-        "value": "1",
-        "suit element": "water",
+    "Cups":{
+        "suit": "Cups", "suit element": "water",
         "suit keywords": "emotion, feelings, habits, healing, intuition, relationships, spirituality",
         "suit duality": "passive, receptive",
         "suit astrology": "cancer, scorpio, pisces",
         "suit alt names": "chalices, goblets, vessels",
-        "suit jung": "feeling",
-        "suit direction": "west",
-        "suit time": "autumn, dusk",
-        "yes-no": "yes",
-        "symbols": [
-            "lotuses in water (enlightenment, rebirth, fertility)",
-            "dove (peace, love, divine inspiration)",
-            "five streams of water (four elements and aether)"
-        ],
-        "upright keywords": "love, new feelings, emotional awakening, creativity, spirituality, intuition, joy, happiness",
-        "reversed keywords": "coldness, emptiness, emotional loss, blocked creativity, feeling unloved, gloominess, sadness"
+        "suit jung": "feeling", "suit direction": "west", "suit time": "autumn, dusk",
+        "cards":{
+            "Ace of Cups":{
+                "value": 1,
+                "yes-no": "yes",
+                "upright keywords": "love, new feelings, emotional awakening, creativity, spirituality, intuition, joy, happiness",
+                "reversed keywords": "coldness, emptiness, emotional loss, blocked creativity, feeling unloved, gloominess, sadness",
+                "symbols": [
+                    "lotuses in water (enlightenment, rebirth, fertility)",
+                    "dove (peace, love, divine inspiration)",
+                    "five streams of water (four elements and aether)"
+                ]
+            },
+            "Two of Cups":{
+                "value": 2,
+                "yes-no": "yes",
+                "upright keywords": "unity, partnership, attraction, connection, close bonds, mutual respect, emotional intimacy",
+                "reversed keywords": "separation, rejection, division, imbalance, tension, bad communication, withdrawal, distance",
+                "symbols": [
+                    "lion (courage, justice)",
+                    "caduceus (balance, healing, harmony)",
+                    "two figures (equal footing, trust)"
+                ]
+            },
+            "Three of Cups":{
+                "value": 3,
+                "yes-no": "yes",
+                "upright keywords": "friendship, community, celebrations, gatherings, social events, emotional fulfillment, support",
+                "reversed keywords": "gossip, scandal, excess, isolation, loneliness, solitude, imbalanced social life, emotional disconnection",
+                "symbols": [
+                    "clear skies (easy times, wisdom)",
+                    "fruits (abundance, joyful gatherings)",
+                    "red robe (passion, vitality)"
+                ]
+            },
+            "Four of Cups":{
+                "value": 4,
+                "yes-no": "maybe",
+                "upright keywords": "apathy, contemplation, discontent, emotional detachment, introspection, lack of excitement, stagnation",
+                "reversed keywords": "clarity, awareness, acceptance, choosing happiness, new beginnings, emotional growth, engagement",
+                "symbols": [
+                    "crossed arms (being closed to others)",
+                    "tree (growth, creation, knowledge)",
+                    "hand (divine intervention, new opportunities)"
+                ]
+            },
+            "Five of Cups":{
+                "value": 5,
+                "yes-no": "no",
+                "upright keywords": "loss, grief, disappointment, sadness, mourning, discontent, self-pity, regret, emotional pain",
+                "reversed keywords": "acceptance, moving on, finding peace, contentment, seeing positives, emotional healing, resilience",
+                "symbols": [
+                    "black cloak (mourning, grief)",
+                    "overturned cups (past failures, negativity)",
+                    "bridge in distance (ability to overcome obstacles)"
+                ]
+            }
+        }
+        
     },
-    "Two of Cups":{
-        "suit": "cups",
-        "value": "2",
-        "suit element": "water",
-        "suit keywords": "emotion, feelings, habits, healing, intuition, relationships, spirituality",
-        "suit duality": "passive, receptive",
-        "suit astrology": "cancer, scorpio, pisces",
-        "suit alt names": "chalices, goblets, vessels",
-        "suit jung": "feeling",
-        "suit direction": "west",
-        "suit time": "autumn, dusk",
-        "yes-no": "yes",
-        "symbols": [
-            "lion (courage, justice)",
-            "caduceus (balance, healing, harmony)",
-            "two figures (equal footing, trust)"
-        ],
-        "upright keywords": "unity, partnership, attraction, connection, close bonds, mutual respect, emotional intimacy",
-        "reversed keywords": "separation, rejection, division, imbalance, tension, bad communication, withdrawal, distance"
+    "Pentacles":{
+        "suit": "Pentacles",
+        "suit element": "",
+        "suit keywords": "",
+        "suit duality": "",
+        "suit astrology": "",
+        "suit alt names": "",
+        "suit jung": "",
+        "suit direction": "",
+        "suit time": ""
     },
-    "Three of Cups":{
-        "suit": "cups",
-        "value": "3",
-        "suit element": "water",
-        "suit keywords": "emotion, feelings, habits, healing, intuition, relationships, spirituality",
-        "suit duality": "passive, receptive",
-        "suit astrology": "cancer, scorpio, pisces",
-        "suit alt names": "chalices, goblets, vessels",
-        "suit jung": "feeling",
-        "suit direction": "west",
-        "suit time": "autumn, dusk",
-        "yes-no": "yes",
-        "symbols": [
-            "clear skies (easy times, wisdom)",
-            "fruits (abundance, joyful gatherings)",
-            "red robe (passion, vitality)"
-        ],
-        "upright keywords": "unity, partnership, attraction, connection, close bonds, mutual respect, emotional intimacy",
-        "reversed keywords": "separation, rejection, division, imbalance, tension, bad communication, withdrawal, distance"
+    "Swords":{
+        "suit": "Pentacles",
+        "suit element": "",
+        "suit keywords": "",
+        "suit duality": "",
+        "suit astrology": "",
+        "suit alt names": "",
+        "suit jung": "",
+        "suit direction": "",
+        "suit time": ""
+    },
+    "Wands":{
+        "suit": "Pentacles",
+        "suit element": "",
+        "suit keywords": "",
+        "suit duality": "",
+        "suit astrology": "",
+        "suit alt names": "",
+        "suit jung": "",
+        "suit direction": "",
+        "suit time": ""
     }
 }
-
