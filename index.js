baae503705148948db9b4289761e9bf32a0fca0e
miltonbanksy <miltonbanksy@gmail.com>
2025-08-28 01:53:05 +0900

    added Joy page button

---
+++ index.js
@@ -0,0 +1,10 @@
+const imgBtnJoy = document.getElementById('frame_flower');
+
+
+// FUNCTIONS:
+
+function openPageJoy(){
+    window.open("joy1.html", "_self");
+}
+// LISTENERS:
+imgBtnJoy.addEventListener('click', openPageJoy);
