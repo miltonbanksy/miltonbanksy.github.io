39abf1877a4200d2ea67d87fddb27ddf5524e164
miltonbanksy <miltonbanksy@gmail.com>
2025-08-28 23:27:41 +0900

    Working on my animal symbollism button

---
+++ animal_symbols.js
@@ -0,0 +1,19 @@
+const animalSymbols = [
+    "Bear: hibernation, new life, resurrection, strength, power, size",
+    "Birds: transition, freedom, liberty, flight, transport",
+    "Buffalo: earth, bravery, freedom, kindness, strength, respect, gratitude, true to one's self, safety, power, durability",
+    "Bull: majesty, virility, sovereignty, wealth, masculine power, connection to the gods",
+    "Butterfly: transformation, significant change, death and rebirth, old self dying, new self emerging",
+    "Cow: earth, power, nurturing, fertility, steadfastness, compassion, perception",
+    "Crocodile: predator, connection to the gods, fierceness",
+    "Crow: magic, mystery, higher perspective, flexibility, trickster, double-edged nature, manipulation",
+    "Deer: majesty, woodland deities, profound change",
+    "Dog: shapeshifter, man's best friend, vigilant, faithful, wise, fidelity, female seduction",
+    "Dove: gentle nature, universal peace, innocence, purity, the soul, Holy Spirit, Holy Grail",
+    "Duck: marital happiness, fidelity, wedding invitations, lucky, feasting, clairvoyance",
+    "Eagle: feared scavenger, death, death-related deities, reconnection with one's spiritual path, embark on a spiritual journey, a reminder to listen to one's heart",
+    "Elephant: large, powerful, battle, military might and triumph",
+    "Fox: sly, cunning, deceitful, devil-like qualities, wealth, fertility, agriculture",
+    "Goat: maintaining life, creating life, fertility, abundance, lust, vitality, creativity, virility",
+    "Grasshopper: constantly on-the-move, take a leap of faith, good luck, good vibes, break confliction, make a decision"
+]
