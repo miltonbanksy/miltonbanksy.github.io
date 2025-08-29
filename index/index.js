771883247b70dbf828684d7fc1401280fd5055ce
miltonbanksy <miltonbanksy@gmail.com>
2025-08-29 14:20:56 +0900

    tidying folders

--- index/index.js
+++ index/index.js
@@ -6,11 +6,11 @@ const imgBtnSteve = document.getElementById('flex-box2');
 // FUNCTIONS:
 
 function openPageJoy(){
-    window.open("joy1.html", "_self");
+    window.open("../joy1/joy1.html", "_self");
 }
 
 function openPageSteve(){
-    window.open('steve1.html', '_self');
+    window.open('../steve1/steve1.html', '_self');
 }
 // LISTENERS:
 imgBtnJoy.addEventListener('click', openPageJoy);
