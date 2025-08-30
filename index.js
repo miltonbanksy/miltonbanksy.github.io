3bf4d2ba2eb5f8982bc7e9f06e42416c06904335
miltonbanksy <miltonbanksy@gmail.com>
2025-08-30 18:36:32 +0900

    index title 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const imgBtnJoy = document.getElementById('flex-box1');
 const imgBtnSteve = document.getElementById('flex-box2');
-
+const imgBtnBlog = document.getElementById('flex-box3');
 
 
 // FUNCTIONS:
@@ -12,6 +12,11 @@ function openPageJoy(){
 function openPageSteve(){
     window.open('../steve1/steve1.html', '_self');
 }
+
+function openPageBlog(){
+    window.open('../stevejoyblog/sj_blog.html');
+}
 // LISTENERS:
 imgBtnJoy.addEventListener('click', openPageJoy);
 imgBtnSteve.addEventListener('click', openPageSteve);
+imgBtnBlog.addEventListener('click', openPageBlog);
