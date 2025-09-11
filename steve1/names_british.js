bad147906e8a50776845214da76cbba0ca5c7181
miltonbanksy <miltonbanksy@gmail.com>
2025-09-12 01:49:03 +0900

    Adding name buttons

---
+++ steve1/names_british.js
@@ -0,0 +1,37 @@
+
+const namesBritishFemales = [
+    "Abigail", "Ada", "Alexandra", "Alice", "Amelia", "Amy", "Anna", "Annabelle", "Aria", "Ariana",
+    "Ava", "Beatrice", "Bethany", "Bonnie", "Brooke", "Camilla", "Carla", "Catherine", "Charlotte", "Chloe",
+    "Clara", "Cleo", "Daisy", "Daphne", "Darcy", "Delilah", "Diana", "Edith", "Eleanor", "Eliza",
+    "Elizabeth", "Ella", "Ellie", "Elsie", "Emily", "Emma", "Erin", "Esme", "Eva", "Evelyn",
+    "Evie", "Faith", "Faye", "Florence", "Freya", "Georgia", "Georgina", "Grace", "Hallie", "Hannah",
+    "Harper", "Harriet", "Hazel", "Heidi", "Holly", "Imogen", "India", "Isabel", "Isabella", "Isla",
+    "Ivy", "Jasmine", "Jessica", "Joanna", "Josephine", "Julia", "Kate", "Katherine", "Katie", "Lara",
+    "Laura", "Lauren", "Layla", "Leah", "Lilly", "Lily", "Lucy", "Luna", "Lydia", "Maisie",
+    "Martha", "Mary", "Matilda", "Maya", "Megan", "Millie", "Molly", "Nancy", "Naomi", "Niamh",
+    "Olivia", "Ophelia", "Orla", "Paige", "Phoebe", "Poppy", "Rebecca", "Rosie", "Ruby", "Sophie"
+]
+const namesBritishMales = [
+    "Adam", "Adrian", "Albie", "Albert", "Alexander", "Alfie", "Alfred", "Andrew", "Angus", "Anthony",
+    "Archie", "Arthur", "Asher", "Austin", "Barney", "Ben", "Benjamin", "Blake", "Bobby", "Bradley",
+    "Brandon", "Callum", "Calvin", "Cameron", "Charles", "Charlie", "Chester", "Christian", "Christopher", "Cian",
+    "Connor", "Daniel", "David", "Dexter", "Dominic", "Dylan", "Edward", "Eli", "Elijah", "Elliot",
+    "Elliott", "Ellis", "Ethan", "Ewan", "Felix", "Finlay", "Finley", "Finn", "Francis", "Freddie",
+    "Frederick", "Gabriel", "George", "Gregory", "Hamish", "Harley", "Harrison", "Harry", "Harvey", "Henry",
+    "Hugo", "Hunter", "Isaac", "Isaiah", "Jack", "Jacob", "Jake", "James", "Jamie", "Jasper",
+    "Jayden", "Jenson", "Joel", "John", "Jonathan", "Joseph", "Joshua", "Jude", "Kai", "Kian",
+    "Kyle", "Laurence", "Leo", "Leon", "Leonard", "Lewis", "Liam", "Logan", "Louis", "Lucas",
+    "Luke", "Malcolm", "Marcus", "Mason", "Matthew", "Max", "Michael", "Milo", "Nathan", "Nicholas"
+]
+const namesBritishSurnames = [
+    "Adams", "Allen", "Anderson", "Armstrong", "Atkinson", "Bailey", "Baker", "Barker", "Barnes", "Bell",
+    "Bennett", "Black", "Blake", "Booth", "Bradley", "Brown", "Bryant", "Burton", "Butler", "Campbell",
+    "Carr", "Carter", "Chapman", "Clark", "Clarke", "Collins", "Cook", "Cooper", "Cox", "Crawford",
+    "Cunningham", "Davies", "Davis", "Dawson", "Day", "Dixon", "Douglas", "Duncan", "Edwards", "Elliott",
+    "Ellis", "Evans", "Fisher", "Fletcher", "Ford", "Foster", "Fox", "Fraser", "Gibson", "Graham",
+    "Grant", "Gray", "Green", "Griffiths", "Hall", "Hamilton", "Harris", "Harrison", "Hart", "Harvey",
+    "Hawkins", "Hayes", "Henderson", "Hill", "Holmes", "Hopkins", "Howard", "Hudson", "Hughes", "Hunt",
+    "Hunter", "Jackson", "James", "Jenkins", "Johnson", "Johnston", "Jones", "Kelly", "Kennedy", "Khan",
+    "King", "Knight", "Lane", "Lawrence", "Lawson", "Lee", "Lewis", "Lloyd", "Martin", "Mason",
+    "Matthews", "Miller", "Mitchell", "Moore", "Morgan", "Morris", "Murphy", "Murray", "Nelson", "Newton"
+]
---
+++ steve1/names_filipino.js
@@ -0,0 +1,38 @@
+const namesFilipinoFemales = [
+    "Althea", "Angel", "Angelica", "Angela", "Ann", "Anna", "Anne", "April", "Arriana", "Aubrey",
+    "Bea", "Bella", "Bernadette", "Bianca", "Blessie", "Camille", "Carla", "Carmina", "Cassandra", "Catherine",
+    "Celeste", "Charmaine", "Chelsea", "Cherry", "Cheska", "Christine", "Clarisse", "Coleen", "Cristine", "Czarina",
+    "Danica", "Danna", "Daphne", "Debbie", "Denise", "Diana", "Divine", "Donna", "Doreen", "Elaine",
+    "Elena", "Eliza", "Ella", "Ellie", "Elvie", "Emerald", "Erika", "Erin", "Faith", "Fatima",
+    "Frances", "Gabriela", "Gelli", "Genevieve", "Georgina", "Giselle", "Glaiza", "Grace", "Gretchen", "Hanna",
+    "Hazel", "Heart", "Heaven", "Iana", "Irene", "Irish", "Isabel", "Isabella", "Jana", "Janelle",
+    "Jasmine", "Jean", "Jessa", "Jessica", "Jewel", "Jillian", "Joanna", "Joy", "Joyce", "Judith",
+    "Julia", "Juliana", "Justine", "Karen", "Kathryn", "Katie", "KC", "Kim", "Krista", "Kristine",
+    "Krystal", "Kyline", "Lara", "Lea", "Leanne", "Liza", "Lorraine", "Louise", "Lovely", "Luna"
+]
+
+const namesFilipinoMales = [
+    "Adrian", "Albert", "Alden", "Alex", "Alfred", "Aljon", "Alvin", "Andre", "Andrew", "Angelo",
+    "Anthony", "Antonio", "Arnel", "Arnold", "Arturo", "Axel", "Ben", "Benedict", "Benjamin", "Bernard",
+    "Bong", "Brandon", "Bryan", "Caleb", "Carl", "Carlo", "Cedric", "Charles", "Christian", "Christopher",
+    "Clarence", "Clark", "CJ", "Cris", "Daniel", "Darren", "Dave", "David", "Dennis", "Dexter",
+    "Dominic", "Don", "Donald", "Drew", "Edward", "Elijah", "Emil", "Emmanuel", "Enrico", "Eric",
+    "Erwin", "Ethan", "Felix", "Ferdinand", "Francis", "Franco", "Fred", "Gabriel", "Garry", "Gavin",
+    "George", "Gerald", "Gian", "Gilbert", "Gio", "Gregory", "Harold", "Harry", "Henry", "Ian",
+    "Iñigo", "Ivan", "Jake", "James", "Jared", "Jasper", "Jay", "Jericho", "Jerome", "Jessie",
+    "Jesus", "Jethro", "Jim", "Jiro", "JJ", "Joey", "John", "Jonathan", "Jordan", "Joseph",
+    "Joshua", "Jude", "Julian", "Justin", "Kenneth", "Kevin", "Kian", "Kirk", "Kyle", "Lance"
+]
+
+const namesFilipinoSurnames = [
+    "Abad", "Abalos", "Abellanosa", "Agbayani", "Agcaoili", "Aguilar", "Agustin", "Alcantara", "Alfonso", "Almario",
+    "Alonzo", "Alvarez", "Amante", "Andres", "Ang", "Aquino", "Araneta", "Arceo", "Arellano", "Asuncion",
+    "Austria", "Avila", "Azores", "Bacani", "Bacolod", "Baisas", "Balagtas", "Balane", "Baluyot", "Bangayan",
+    "Bao", "Baranda", "Barrientos", "Basco", "Bautista", "Belmonte", "Benitez", "Bermudez", "Bernal", "Bernardo",
+    "Bonifacio", "Borja", "Briones", "Buendia", "Cabrera", "Calderon", "Camacho", "Canlas", "Capili", "Cariaga",
+    "Carpio", "Castillo", "Castro", "Cayetano", "Ceniza", "Chavez", "Chua", "Climaco", "Concepcion", "Constantino",
+    "Corpuz", "Cruz", "Cuevas", "Dalisay", "Dela Cruz", "Dela Peña", "Del Rosario", "Delos Reyes", "Diaz", "Dimaculangan",
+    "Domingo", "Dumlao", "Dy", "Espino", "Espiritu", "Estrella", "Estrada", "Fabian", "Fajardo", "Fernandez",
+    "Fernando", "Flores", "Fortun", "Francisco", "Fuentes", "Gamboa", "Garcia", "Gascon", "Gonzales", "Gonzalez",
+    "Guerrero", "Guevarra", "Gutierrez", "Hernandez", "Herrera", "Ibanez", "Ignacio", "Ilagan", "Imperial", "Jacinto"
+]
