40f064d60c413afd69fb8fced2dde9486c57e7f5
miltonbanksy <miltonbanksy@gmail.com>
2025-09-21 23:17:13 +0900

    stable

--- joy1/joy_hangman/hangman_ldd_characters.js
+++ joy1/joy_hangman/hangman_ldd_characters.js
@@ -1,5 +1,17 @@
 const dictDollNames = {
-      damien:{
+    bride_of_valentine: {
+  name: "Bride of Valentine",
+  img: "images/bov.webp",
+  poem: `She holds for you
+In her hand,
+A broken heart,
+That has been damned.`,
+  epitaph: `This cold unloved bride,
+Was stitched together with spare parts.
+The only thing they left out of her,
+Was a warm, still beating heart`
+},
+    damien:{
         name: "Damien",
         img: "images/damien.webp",
         poem: `This little boy has a trick,
@@ -10,8 +22,8 @@ And three books to read bleed.`,
 With a soul as empty as his eyes,
 This wee little lad pronounced D.O.A.,
 Was already dead inside.`
-      },
-      deadbra_ann: {
+    },
+    deadbra_ann: {
         name: "Deadbra Ann",
         img: "images/Deadbra-ann.webp",
         poem: `Our prom queen comes,
@@ -22,8 +34,8 @@ and ready to scare ya.`,
 Turned queen to carcass by morning light.
 Deadbra Ann is back without a breath,
 To turn each night into a dance of death.`
-      },
-      eggzorcist: {
+    },
+    eggzorcist: {
         name: "Eggzorcist",
         img: "images/Eggzorcist.webp",
         poem:`This little bunny,
@@ -34,8 +46,20 @@ Are her favorite treats.`,
 Used to fasten ears of a rabbit,
 Put this little bunny,
 In a kiddie casket.`
-      },
-      kitty: {
+    },
+    inferno: {
+        name: "Inferno",
+        img: "images/Inferno.webp",
+        poem: `Like a Phoenix,
+From the flames she arose.
+Pain and suffering,
+She now bestows.`,
+        epitaph: `Tiny Inferno liked to play with black magic,
+Until she met her demise so tragic.
+She conjured a demon she could not control,
+Angry and starving he devoured her soul.`
+    },
+    kitty: {
         name:"Kitty",
         img: "images/Kitty.webp",
         poem: `This little girl,
@@ -46,8 +70,20 @@ She's ready to spread fear.`,
 V - is for vengeance and unholy pride.
 I - is for infidels shall be eaten by ghouls.
 L - is for lustfully disposing of fools`
-      },
-      lizzie_borden: {
+    },
+    lilith: {
+        name: "Lilith",
+        img: "images/Lilith.webp",
+        poem: `Only a stake or the sun,
+Will kill this beast.
+For on your blood,
+She will feast.`,
+        epitaph: `Lilith loved all things goth,
+Which forced her way into eternal sloth.
+Now she's damned to wake each night,
+To feast on blood till morning light.`
+    },
+    lizzie_borden: {
         name: "Lizzie Borden",
         img: "images/Lizzieborden.webp",
         poem: `This lovely child,
@@ -58,8 +94,20 @@ And on the attack.`,
 And gave her mother forty whacks.
 And when she saw what she had done,
 She gave her father forty-one.`
-      },
-      lou_sapphire: {
+    },
+    lottie: {
+        name: "Lottie",
+        img: "images/Lottie.webp",
+        poem: `She comes with an umbrella,
+To help keep out the sun.
+Now that everything is dark,
+Her playtime has begun.`,
+        epitaph: `Lottie ate a pound of aspirin,
+That went straight to her head.
+She pretended they were sweets,
+Instead of treats for the dead.`
+    },
+    lou_sapphire: {
         name:"Lou Sapphire",
         img: "images/Lousapphire.webp",
         poem: `A cane, a contract,
@@ -70,8 +118,8 @@ And hell comes with him.`,
 But has returned.
 His clothes quite cool,
 Now souls will burn.`
-      },
-      posie: {
+    },
+    posie: {
         name: "Posie",
         img: "images/Posey.webp",
         poem: `This little girl is
@@ -82,8 +130,8 @@ Bloodied with rage.`,
 Was buried alive.
 Some things won't stay dead,
 Even after they've died.`
-      },
-      sadie: {
+    },
+    sadie: {
         name: "Sadie",
         img: "images/Sadie.webp",
         poem: `Watch this little one,
@@ -95,8 +143,32 @@ And black bouquet.`,
 So Sadie chose to rise instead.
 Being easily forgotten wouldn't be fair,
 Death was something this child had to share.`
-      },
-      sin: {
+    },
+    schitzo: {
+        name: "Schitzo",
+        img: "images/Schitzo.webp",
+        poem: `He'll hand out balloons,
+Then lop off your head.
+He's playful and witty,
+And also quite dead.`,
+        epitaph: `On the midway of Hell,
+Schitzo died of laughter.
+Now he turns each show,
+Into a bloody disaster.`
+    },
+    sheena: {
+        name: "Sheena",
+        img: "images/Sheena.webp",
+        poem: `Risen from the ashes,
+Of a furious sound.
+This little one,
+Will always remain underground.`,
+epitaph: `Diving off the stage,
+Sheena landed on her head.
+She has risen from the grave,
+Because punk's not dead.`
+},
+    sin: {
         name: "Sin",
         img: "images/Sin.webp",
         poem: `This little devil has a
