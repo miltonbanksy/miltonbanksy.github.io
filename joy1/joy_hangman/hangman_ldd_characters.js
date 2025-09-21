0617ba53cfd1f2d8658c687da70c0da368db94dc
miltonbanksy <miltonbanksy@gmail.com>
2025-09-22 00:01:44 +0900

    added more characters

--- joy1/joy_hangman/hangman_ldd_characters.js
+++ joy1/joy_hangman/hangman_ldd_characters.js
@@ -1,16 +1,38 @@
 const dictDollNames = {
     bride_of_valentine: {
-  name: "Bride of Valentine",
-  img: "images/bov.webp",
-  poem: `She holds for you
+        name: "Bride of Valentine",
+        img: "images/bov.webp",
+        poem: `She holds for you
 In her hand,
 A broken heart,
 That has been damned.`,
-  epitaph: `This cold unloved bride,
+        epitaph: `This cold unloved bride,
 Was stitched together with spare parts.
 The only thing they left out of her,
 Was a warm, still beating heart`
-},
+    },
+    calico: {
+        name: "Calico",
+        img: "images/calico.webp",
+        poem: `Calico sure loves her pet Muzzy.
+Some parts are scaly,
+And some parts are fuzzy.`,
+        epitaph: `Calico was a puzzle to everyone she met,
+A mystery to be solved, but never to forget.
+She always chose to keep her body modified,
+Stitched together jigsaw, unliving zombified.`
+    },
+    dahlia: {
+        name: "Dahlia",
+        img: "images/dahlia.webp",
+        poem: `Little Dahlia wanted to be on the big screen,
+As she would find out the scene was quite mean.
+On the morning they found her she had been cut in two,
+Just who had done this nobody knew.`,
+        epitaph: `Name of Deceased - Dahlia
+Last Occupation - Odd Jobs
+Cause of Death - Bisection`
+    },
     damien:{
         name: "Damien",
         img: "images/damien.webp",
@@ -35,6 +57,17 @@ Turned queen to carcass by morning light.
 Deadbra Ann is back without a breath,
 To turn each night into a dance of death.`
     },
+    dottie_rose: {
+        name: "Dottie Rose",
+        img: "images/dottie_rose.webp",
+        poem: `Dottie Rose and her pet poodle Hun.
+As dead as they are,
+They still have some fun.`,
+        epitaph: `Dottie was highly allergic to the sun,
+But she loved sundresses and her pet poodle Hun.
+Late one morning Hun ran out into the day,
+Dottie followed fast, dying on her way.`
+    },
     eggzorcist: {
         name: "Eggzorcist",
         img: "images/Eggzorcist.webp",
@@ -47,6 +80,31 @@ Used to fasten ears of a rabbit,
 Put this little bunny,
 In a kiddie casket.`
     },
+    hollywood: {
+        name: "Hollywood",
+        img: "images/hollywood.webp",
+        poem: `Hollywood was a famous glamour gal.
+She liked to race around with her rich promoter Sal.
+But late one night this resulted in their death,
+For the brakes have been cut by a stalker named Seth.`,
+        epitaph: `Name of Deceased - Hollywood
+Last Occupation - Actress
+Cause of Death - Massive Head Trauma.`
+    },
+    hush: {
+  name: "Hush",
+  img: "images/hush.webp",
+  poem: `Shy little Hush,
+  And her pet rat Shriek.
+Dwelling in the sewer,
+These two are sure to reek.`,
+  epitaph: `Shy little Hush thought rats were so pretty,
+So it was her good fortune she lived by the city.
+It was easy to sneak into the sewer each night,
+To offer her pets a cheesy delight.
+But one silly slip landed Hush on her head,
+It was right there she drowned and the rats were well fed.`
+},
     inferno: {
         name: "Inferno",
         img: "images/Inferno.webp",
@@ -58,6 +116,40 @@ She now bestows.`,
 Until she met her demise so tragic.
 She conjured a demon she could not control,
 Angry and starving he devoured her soul.`
+    },
+    isaac: {
+        name: "Isaac",
+        img: "images/isaac.webp",
+        poem: `Isaac is a scarecrow who wants to steal your soul,
+But he is too busy hanging and loving his Ole' Crow.`,
+        epitaph: `Nailed onto a cross,
+This false prophet hates.
+Crows pecking on his innards,
+He just hangs and waits.`
+    },
+    jezebel: {
+        name: "Jezebel",
+        img: "images/jezebel.webp",
+        poem: `Jezebel was a runaway who lived on the street,
+Saddened by the sound of her own heartbeat.
+With a razor in hand it was all just too much,
+Her body fell fast, ice cold to the touch.`,
+        epitaph: `Name of Deceased - Jezebel
+Last Occupation - Entertainer
+Cause of Death - Loss of Blood`
+    },
+    jinx: {
+        name: "Jinx",
+        img: "images/jinx.webp",
+        poem: `Jinx and her Hellcat make quite a pair.
+Their path is paved with bad luck,
+So cross it if you dare.`,
+        epitaph: `Breaking mirrors
+And walking under ladders
+Till she died,
+This unlucky kitten
+Used up
+All of her nine lives.`
     },
     kitty: {
         name:"Kitty",
@@ -119,6 +211,42 @@ But has returned.
 His clothes quite cool,
 Now souls will burn.`
     },
+    lulu: {
+        name: "Lulu",
+        img: "images/Lulu.webp",
+        poem: `With a broken leg,
+And roller skates.
+She wants to be,
+Your new playmate.`,
+        epitaph: `She skated towards the concrete stairs,
+Hearing the many shouted dares.
+Then she tumbled straight to Hell,
+Into her own little grave our dear Lulu fell.`
+    },
+    macumba: {
+        name: "Macumba",
+        img: "images/macumba.webp",
+        poem: `With a doll,
+He casts his spell.
+No one is safe
+From this zombie hell.`,
+        epitaph: `Placed under a voodoo curse,
+It was an evil spell he could not reverse.
+A soul trapped behind lifeless eyes,
+All things alive are now despised.`
+    },
+    ms_eerie: {
+        name: "Ms Eerie",
+        img: "images/ms_eerie.webp",
+        poem: `Behind a veil,
+Blank eyes stare.
+At a funeral wreath,
+Full of despair.`,
+        epitaph: `Lonely Ms. Eerie,
+Lies in her coffin alone.
+Little does she realize,
+The funeral she attends is her own.`
+    },
     posie: {
         name: "Posie",
         img: "images/Posey.webp",
@@ -130,6 +258,17 @@ Bloodied with rage.`,
 Was buried alive.
 Some things won't stay dead,
 Even after they've died.`
+    },
+    revenant: {
+        name: "Revenant",
+        img: "images/revenant.webp",
+        poem: `One hell of a ghost and Carrion her pet.
+Are two of the creepiest ghouls,
+You wish you never met.`,
+        epitaph: `An image from the past, she roams eternally,
+Reliving the horrid night of a twisted tragedy.
+To gaze upon this ghost, you'll witness her demise,
+For she is a harbinger of death as you will surely realize.`
     },
     sadie: {
         name: "Sadie",
@@ -179,5 +318,39 @@ Instead of little bruises.`,
 Consumed by flames at seven.
 Better to forever serve in hell,
 than grow up and play in heaven.`
-      }
+      },
+      siren: {
+  name: "Siren",
+  img: "images/siren.webp",
+  poem: `Siren was an untimely singer,
+Whose voice it was said sure could linger.
+Until the night she was sewn into silence,
+Her song abruptly ended with a fierce act of violence.`,
+  epitaph: `Name of Deceased - Siren
+Last Occupation - Vocalist
+Cause of Death - Mouth sewn shut`
+},
+      sybil: {
+        name: "Sybil",
+        img: "images/Sybil.webp",
+        poem: `Strapped in a straight jacket,
+with a collar and chain.
+Pray that this is enough,
+to keep Sybil restrained.`,
+        epitaph: `Locked away forever insane,
+The doctors came and removed her brain.
+She died on the table by morning light,
+Now in search of a mind she wanders the night.`
+    },
+    vincent_vaude: {
+        name: "Vincent Vaude",
+        img: "images/vincent_vaude.webp",
+        poem: `He dreamed of performing on the grand old stage,
+An amazing escape artist of a tender young age.
+So small Vincent Vaude locked himself in a trunk,
+By the time someone found him his body sure stunk.`,
+        epitaph: `Name of Deceased - Vincent Vaude
+Last Occupation - Escape Artist
+Cause of Death - Suffocation`
+    },
     } // End of objDollNames
