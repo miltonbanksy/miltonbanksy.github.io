60426aab07889cd84776fa63f96997b64c7569d6
miltonbanksy <miltonbanksy@gmail.com>
2025-09-21 19:22:52 +0900

    hangman

---
+++ joy1/joy_hangman/hangman_ldd_characters.js
@@ -0,0 +1,75 @@
+const dictDollNames = {
+      damien:{
+        name: "Damien",
+        img: "images/damien.webp",
+        poem: `This little boy has a trick,
+Up his sleeve.
+At least one slingshot,
+And three books to read bleed.`,
+        epitaph: `And then there was Damien,
+With a soul as empty as his eyes,
+This wee little lad pronounced D.O.A.,
+Was already dead inside.`
+      },
+      eggzorcist: {
+        name: "Eggzorcist",
+        img: "images/Eggzorcist.webp",
+        poem:`This little bunny,
+Is digging for sweets.
+Roaches in baskets,
+Are her favorite treats.`,
+      epitaph: `A drawstring,
+Used to fasten ears of a rabbit,
+Put this little bunny,
+In a kiddie casket.`
+      },
+      kitty: {
+        name:"Kitty",
+        img: "images/Kitty.webp",
+        poem: `This little girl,
+Wants to give you a cheer.
+With pompoms and bull horn,
+She's ready to spread fear.`,
+        epitaph: `E - is for everything hateful inside.
+V - is for vengeance and unholy pride.
+I - is for infidels shall be eaten by ghouls.
+L - is for lustfully disposing of fools`
+      },
+      posie: {
+        name: "Posie",
+        img: "images/Posey.webp",
+        poem: `This little girl is
+Out of the grave
+With one simple rose,
+Bloodied with rage.`,
+        epitaph: `Poor little Posey
+Was buried alive.
+Some things won't stay dead,
+Even after they've died.`
+      },
+      sadie: {
+        name: "Sadie",
+        img: "images/Sadie.webp",
+        poem: `Watch this little one,
+She's pining away,
+With her butcher's knife,
+Coffin purse
+And black bouquet.`,
+        epitaph: `No one cared how she turned up dead.
+So Sadie chose to rise instead.
+Being easily forgotten wouldn't be fair,
+Death was something this child had to share.`
+      },
+      sin: {
+        name: "Sin",
+        img: "images/Sin.webp",
+        poem: `This little devil has a
+Pitchfork she uses.
+She gives you a poke,
+Instead of little bruises.`,
+        epitaph: `Sweet little Sin,
+Consumed by flames at seven.
+Better to forever serve in hell,
+than grow up and play in heaven.`
+      }
+    } // End of objDollNames
