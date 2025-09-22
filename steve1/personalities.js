c7aa371d5321992187c75af49b0dc9130f569e0b
miltonbanksy <miltonbanksy@gmail.com>
2025-09-22 14:48:41 +0900

    added personalities.js

---
+++ steve1/personalities.js
@@ -0,0 +1,18 @@
+const personalities = [
+    "Analyst / Advisor / Statistician - Architect / Designer: Imaginative and strategic thinkers, with a plan for everything.",
+    "Analyst / Advisor / Statistician - Logician / Examiner: Innovative inventors with an unquenchable thirst for knowledge.",
+    "Analyst / Advisor / Statistician - Commander / Leader: Bold, imaginative and strong-willed leaders, always finding a way, or making one.",
+    "Analyst / Advisor / Statistician - Debater / Orator: Smart and curious thinkers who cannot resist an intellectual challenge.",
+    "Diplomat / Official / Negotiator - Advocate / Supporter: Quiet and mystical, yet very inspiring and tireless idealists.",
+    "Diplomat / Official / Negotiator - Mediator / Referee: Poetic, kind and altruistic people, always eager to help a good cause.",
+    "Diplomat / Official / Negotiator - Protagonist / Star: Charismatic and inspiring leaders, able to mesmerize their listeners.",
+    "Diplomat / Official / Negotiator - Campaigner / Promoter: Enthusiastic, creative and sociable free spirits, who can always find a reason to smile.",
+    "Sentinel / Soldier / Guard - Logistician / Overseeer: Practical and fact-minded individuals, whose reliability cannot be doubted.",
+    "Sentinel / Soldier / Guard - Defender / Preserver: Very dedicated and warm protectors, always ready to defend their loved ones.",
+    "Sentinel / Soldier / Guard - Executive / Decision-Maker: Excellent administrators, unsurpassed at managing things or people.",
+    "Sentinel / Soldier / Guard - Consul / Assistant: Extraordinarily caring, social and popular people, always eager to help.",
+    "Explorer / Scout / Pioneer - Virtuoso / Artist: Bold and practical experimenters, masters of all kinds of tools.",
+    "Explorer / Scout / Pioneer - Adventurer / Risk-Taker: Flexible and charming artists, always ready to explore and experience something new.",
+    "Explorer / Scout / Pioneer - Enterpreneur / Profiteer: Smart, energetic and very perceptive people, who truly enjoy living on the edge.",
+    "Explorer / Scout / Pioneer - Entertainer / Performer: Spontaneous, energetic and enthusiastic people. Life is never boring around them."
+];
