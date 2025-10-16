3383443497f8b37ba19ef7cfcff7424de1170276
miltonbanksy <miltonbanksy@gmail.com>
2025-10-16 15:02:14 +0900

    Added Personalities

--- steve1/personalities.js
+++ steve1/personalities.js
@@ -16,3 +16,150 @@ const personalities = [
     "Explorer - Enterpreneur: Smart, energetic and very perceptive people, who truly enjoy living on the edge.",
     "Explorer - Entertainer: Spontaneous, energetic and enthusiastic people. Life is never boring around them."
 ];
+
+const personalities2 = [
+    {
+        type: "architect",
+        class: "analyst",
+        energy: "introverted",
+        mind: "intuitive",
+        nature: "thinking",
+        tactics: "judging",
+        summary: "Imaginative and strategic thinkers, with a plan for everything."
+    },
+    {
+        type: "logician",
+        class: "analyst",
+        energy: "introverted",
+        mind: "intuitive",
+        nature: "thinking",
+        tactics: "prospecting",
+        summary: "Innovative inventors with an unquenchable thirst for knowledge."
+    },
+    {
+        type: "commander",
+        class: "analyst",
+        energy: "extroverted",
+        mind: "intuitive",
+        nature: "thinking",
+        tactics: "judging",
+        summary: "Bold, imaginative and strong-willed leaders, always finding a way...or making one."
+    },
+    {
+        type: "debater",
+        class: "analyst",
+        energy: "extroverted",
+        mind: "intuitive",
+        nature: "thinking",
+        tactics: "prospecting",
+        summary: "Smart and curious thinkers who cannot resist an intellectual challenge."
+    },
+    {
+        type: "advocate",
+        class: "diplomat",
+        energy: "introverted",
+        mind: "intuitive",
+        nature: "feeling",
+        tactics: "judging",
+        summary: "Quiet and mystical, yet very inspiring and tireless idealists."
+    },
+    {
+        type: "mediator",
+        class: "diplomat",
+        energy: "introverted",
+        mind: "intuitive",
+        nature: "feeling",
+        tactics: "prospecting",
+        summary: "Poetic, kind and altruistic people, always eager to help a good cause."
+    },
+    {
+        type: "protagonist",
+        class: "diplomat",
+        energy: "extroverted",
+        mind: "intuitive",
+        nature: "feeling",
+        tactics: "judging",
+        summary: "Charismatic and inspiring leaders, able to mesmerize their listeners."
+    },
+    {
+        type: "campaigner",
+        class: "diplomat",
+        energy: "extroverted",
+        mind: "intuitive",
+        nature: "feeling",
+        tactics: "prospecting",
+        summary: "Enthusiastic, creative and sociable free spirits, who can always find a reason to smile."
+    },
+    {
+        type: "logistician",
+        class: "sentinel",
+        energy: "introverted",
+        mind: "observant",
+        nature: "thinking",
+        tactics: "judging",
+        summary: "Practical and fact-minded individuals, whose reliability cannot be doubted."
+    },
+    {
+        type: "defender",
+        class: "sentinel",
+        energy: "introverted",
+        mind: "observant",
+        nature: "feeling",
+        tactics: "judging",
+        summary: "Very dedicated and warm protectors, always ready to defend their loved ones."
+    },
+    {
+        type: "executive",
+        class: "sentinel",
+        energy: "extroverted",
+        mind: "observant",
+        nature: "thinking",
+        tactics: "judging",
+        summary: "Excellent administrators, unsurpassed at managing things...or people."
+    },
+    {
+        type: "consul",
+        class: "sentinel",
+        energy: "extroverted",
+        mind: "observant",
+        nature: "feeling",
+        tactics: "judging",
+        summary: "Extraordinarily caring, social and popular people, always eager to help."
+    },
+    {
+        type: "virtuoso",
+        class: "explorer",
+        energy: "introverted",
+        mind: "observant",
+        nature: "thinking",
+        tactics: "prospecting",
+        summary: "Bold and practical experimenters, masters of all kinds of tools."
+    },
+    {
+        type: "adventurer",
+        class: "explorer",
+        energy: "introverted",
+        mind: "observant",
+        nature: "feeling",
+        tactics: "prospecting",
+        summary: "Flexible and charming artists, always ready to explore and experience something new."
+    },
+    {
+        type: "entrepreneur",
+        class: "explorer",
+        energy: "extroverted",
+        mind: "observant",
+        nature: "thinking",
+        tactics: "prospecting",
+        summary: "Smart, energetic and very perceptive people, who truly enjoy living on the edge."
+    },
+    {
+        type: "entertainer",
+        class: "explorer",
+        energy: "extroverted",
+        mind: "observant",
+        nature: "feeling",
+        tactics: "prospecting",
+        summary: "Spontaneous, energetic and enthusiastic people. Life is never boring around them."
+    }
+]
