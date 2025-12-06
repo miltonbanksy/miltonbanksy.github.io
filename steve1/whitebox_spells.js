b7ebf9d0a256cb0b2db37509efe8dead9fc9c035
miltonbanksy <miltonbanksy@gmail.com>
2025-12-06 22:58:11 +0900

    Delving the depths of odd

---
+++ steve1/whitebox_spells.js
@@ -0,0 +1,47 @@
+const spellsCleric = [
+    {name: "Cure Light Wounds or Cause Light Wounds", level: 1, range: "Touch", duration: "Instant", description: "Cures 1d6+1 HP / Causes 1d6+1 HP."},
+    {name: "Detect Chaos or Detect Law", level: 1, range: "120ft", duration: "1 hour", description: "Detect Chaotic creatures and evil enchantments, intentions, thoughs, auras. / Detect Lawful..."},
+    {name: "Bless or Curse", level: 2, range: "Target Character", duration: "1 hour", description: "The recipient of this spell cannot be in combat when it is cast. Bless: the recipient gets a +1 bonus to all attack rolls and, if the target is not a Player Character, improved overall morale. / Curse: the recipient suffers a -1 penalty to all attack rolls and, if the target is not a Player Character, decreased morale."},
+    {name: "Find Traps", level: 2, range: "30ft", duration: "Instant", description: "Reveals both magical and non-magical traps."},
+    //{name: "", level: 1, range: "", duration: "", description: ""}
+]
+
+const spellsClericL1 = [
+    "Cure or Cause Light Wounds",
+    "Detect Chaos or Law",
+    "Detect Magic",
+    "Light or Dark",
+    "Protection from Chaos or Law",
+    "Purify or Putrefy Food & Drink"
+];
+
+const spellsClericL2 = [
+    "Bless or Curse",
+    "Find Traps",
+    "Hold Person",
+    "Speak with Animals"
+];
+
+const spellsClericL3 = [
+    "Cure or Cause Disease",
+    "Light or Dark, Continual",
+    "Locate Object",
+    "Remove Curse"
+];
+
+const spellsClericL4 = [
+    "Cure or Cause Serious Wounds",
+    "Neutralize Poison",
+    "Protection from Chaos or Law, 10ft radius",
+    "Speak with Plants",
+    "Sticks to Snakes"
+];
+
+const spellsClericL5 = [
+    "Commune",
+    "Create Food & Drink",
+    "Dispel Chaos or Law",
+    "Insect Plague",
+    "Quest",
+    "Raise Dead"
+];
---
+++ steve1/whitebox_treasure.js
@@ -0,0 +1,50 @@
+const oddTreasurePotions = [
+    { start: 1, end: 3, value: "Animal Control"},
+    { start: 4, end: 6, value: "Clairaudience"},
+    { start: 7, end: 9, value: "Clairvoyance"},
+    { start: 10, end: 12, value: "Diminution"},
+    { start: 13, end: 15, value: "Dragon Control"},
+    { start: 16, end: 18, value: "Ethereality"},
+    { start: 19, end: 21, value: "Fire Resistance"},
+    { start: 22, end: 24, value: "Flying"},
+    { start: 25, end: 27, value: "Gaseous Form"},
+    { start: 28, end: 30, value: "Giant Strength"},
+    { start: 31, end: 33, value: "Growth"},
+    { start: 34, end: 36, value: "Heroism"},
+    { start: 37, end: 39, value: "Invisibility"},
+    { start: 40, end: 42, value: "Invulnerability"},
+    { start: 43, end: 45, value: "Levitation"},
+    { start: 46, end: 48, value: "Plant Control"},
+    { start: 49, end: 55, value: "Poison"},
+    { start: 56, end: 58, value: "Slipperiness"},
+    { start: 59, end: 61, value: "Treasure Finding"},
+    { start: 62, end: 64, value: "Undead Control"},
+    { start: 65, end: 75, value: "Extra Healing"},
+    { start: 76, end: 100, value: "Healing"}
+];
+
+// Not used. Using if, else if instead
+/*
+const oddTreasureScrolls = [
+    {value: 1, descr: "x1 1st level spell"},
+    {value: 2, descr: "x1 1st level spell"},
+    {value: 3, descr: "x1 1st level spell"},
+    {value: 4, descr: "x1 1st level spell"},
+    {value: 5, descr: "x1 1st level spell"},
+    {value: 6, descr: "x1 1st level spell"},
+    {value: 7, descr: "x1 1st level spell"},
+    {value: 8, descr: "x1 1st level spell"},
+    {value: 9, descr: "x1 1st level spell"},
+    {value: 10, descr: "x1 1st level spell"},
+    {value: 11, descr: "x1 1st level spell"},
+    {value: 12, descr: "x1 1st level spell"},
+    {value: 13, descr: "x1 1st level spell"},
+    {value: 14, descr: "x1 1st level spell"},
+    {value: 15, descr: "x1 1st level spell"},
+    {value: 16, descr: "x1 1st level spell"},
+    {value: 17, descr: "x1 1st level spell"},
+    {value: 18, descr: "x1 1st level spell"},
+    {value: 19, descr: "x1 1st level spell"},
+    {value: 20, descr: "x1 1st level spell"}
+];
+*/
