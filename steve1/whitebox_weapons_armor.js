81ce76911023485a7d2cd19db6d9620d37814536
miltonbanksy <miltonbanksy@gmail.com>
2025-12-09 01:55:47 +0900

    Learned about compact code

--- steve1/whitebox_weapons_armor.js
+++ steve1/whitebox_weapons_armor.js
@@ -1,28 +1,78 @@
 const magicalWeaponsAndArmor = [
-    {roll: 1, modifier: "Mod to be confirmed", type: "Cursed Armor or Shield"},
-    {roll: 2, modifier: 1, type: "+1 Missile Weapon(s)"},
-    {roll: 3, modifier: 1, type: "+1 Shield"},
-    {roll: 4, type: "+1 Melee Weapon"},
-    {roll: 5, type: "+1 Melee Weapon"},
-    {roll: 6, type: "+1 Melee Weapon"},
-    {roll: 7, type: "+1 Armor"},
-    {roll: 8, type: "+1 Armor"},
-    {roll: 9, type: "Cursed Weapon"},
-    {roll: 10, type: "+2 Missile Weapon(s)"},
-    {roll: 11, type: ""},
-    {roll: 12, type: ""},
-    {roll: 13, type: ""},
-    {roll: 14, type: ""},
-    {roll: 15, type: ""},
-    {roll: 16, type: ""},
-    {roll: 17, type: ""},
-    {roll: 18, type: ""},
-    {roll: 19, type: ""},
-    {roll: 20, type: ""}
+    {roll: 1, modifier: 0, type: "Cursed Armor or Shield", other: "none"},
+    {roll: 2, modifier: 1, type: "+1 Missile Weapon(s)", other: "none"},
+    {roll: 3, modifier: 1, type: "+1 Shield", other: "none"},
+    {roll: 4, modifier: 1, type: "+1 Melee Weapon", other: "none"},
+    {roll: 5, modifier: 1, type: "+1 Melee Weapon", other: "none"},
+    {roll: 6, modifier: 1, type: "+1 Melee Weapon", other: "none"},
+    {roll: 7, modifier: 1, type: "+1 Armor", other: "none"},
+    {roll: 8, modifier: 1, type: "+1 Armor", other: "none"},
+    {roll: 9, modifier: 0, type: "Cursed Weapon", other: "none"},
+    {roll: 10, modifier: 2, type: "+2 Missile Weapon(s)", other: "none"},
+    {roll: 11, modifier: 2, type: "+2 Shield", other: "none"},
+    {roll: 12, modifier: 2, type: "+2 Melee Weapon", other: "none"},
+    {roll: 13, modifier: 2, type: "+2 Armor", other: "none"},
+    {roll: 14, modifier: 1, type: "+1 Melee Weapon with Minor Ability", other: "Minor Ability"},
+    {roll: 15, modifier: 3, type: "+3 Missile Weapon)s)", other: "none"},
+    {roll: 16, modifier: 3, type: "+3 Melee Weapon", other: "none"},
+    {roll: 17, modifier: 3, type: "+3 Shield", other: "none"},
+    {roll: 18, modifier: 3, type: "+3 Armor", other: "none"},
+    {roll: 19, modifier: 0, type: "Unusual Weapon (optional) or Re-roll", other: "none"},
+    {roll: 20, modifier: 0, type: "Unusual Armor (optional) or Re-roll", other: "none"}
 ];
+
+const cursedArmorWeaponsSheilds = [
+    {roll: 1, modifier: -1, type: "-1 Weapon or Armor"},
+    {roll: 2, modifier: -1, type: "-1 Weapon or Armor"},
+    {roll: 3, modifier: -1, type: "-1 Weapon or Armor"},
+    {roll: 4, modifier: -2, type: "-2 Weapon or Armor"},
+    {roll: 5, modifier: -3, type: "-3 Weapon or Armor"},
+    {roll: 6, modifier: 0, type: "Attracts missiles - Grants a +1 to-hit bonus to attack on such missiles."}
+];
+
+const MinorAbilitiesForMeleeWeapons = [
+    {roll: 1, type: "Additional damage (+1)"},
+    {roll: 2, type: "Additional damage (+1)"},
+    {roll: 3, type: "Additional damage (+1)"},
+    {roll: 4, type: "Additional damage (+1)"},
+    {roll: 5, type: "Sheds light, 15 ft. radius"},
+    {roll: 6, type: "Sheds light, 30 ft. radius"}
+]
 
 const magicMeleeWeapons = [
     {roll: 1, type: "Axe, battle"},
     {roll: 2, type: "Axe, hand"},
     {roll: 3, type: "Dagger"},
+    {roll: 4, type: "Lance"},
+    {roll: 5, type: "Mace, heavy"},
+    {roll: 6, type: "Mace, light"},
+    {roll: 7, type: "Spear"},
+    {roll: 8, type: "Staff"},
+    {roll: 9, type: "Sword, long"},
+    {roll: 10, type: "Sword, short"},
+    {roll: 11, type: "Sword, two-handed"},
+    {roll: 12, type: "War Hammer"}
+];
+
+const magicMissileWeapons = [
+    {roll: 1, type: "2d6 Arrows"},
+    {roll: 2, type: "2d6 Arrows"},
+    {roll: 3, type: "2d6 Arrows"},
+    {roll: 4, type: "2d6 Arrows"},
+    {roll: 5, type: "2d6 Arrows"},
+    {roll: 6, type: "2d6 Arrows"},
+    {roll: 7, type: "2d6 Arrows"},
+    {roll: 8, type: "2d6 Arrows"},
+    {roll: 9, type: "1d10 Stones"},
+    {roll: 10, type: "1d10 Stones"},
+    {roll: 11, type: "Spear"},
+    {roll: 12, type: "2d4 Darts"},
+    {roll: 13, type: "2d4 Darts"},
+    {roll: 14, type: "2d4 Darts"},
+    {roll: 15, type: "2d4 Darts"},
+    {roll: 16, type: "2d6 Bolts, crossbow"},
+    {roll: 17, type: "2d6 Bolts, crossbow"},
+    {roll: 18, type: "2d6 Bolts, crossbow"},
+    {roll: 19, type: "2d6 Bolts, crossbow"},
+    {roll: 20, type: "2d6 Bolts, crossbow"},
 ];
