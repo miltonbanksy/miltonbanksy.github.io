e55ad2419afc5272d8074fb3c1b7907ad765d1a9
miltonbanksy <miltonbanksy@gmail.com>
2025-12-10 01:06:59 +0900

    starting misc items

--- steve1/whitebox_weapons_armor.js
+++ steve1/whitebox_weapons_armor.js
@@ -1,6 +1,6 @@
 const magicalWeaponsAndArmor = [
     {roll: 1, modifier: 0, type: "Cursed Armor or Shield", other: "none"},
-    {roll: 2, modifier: 1, type: "+1 Missile Weapon(s)", other: "none"},
+    {roll: 2, modifier: 1, type: "Missile Weapon(s)", other: "none"},
     {roll: 3, modifier: 1, type: "+1 Shield", other: "none"},
     {roll: 4, modifier: 1, type: "+1 Melee Weapon", other: "none"},
     {roll: 5, modifier: 1, type: "+1 Melee Weapon", other: "none"},
@@ -13,7 +13,7 @@ const magicalWeaponsAndArmor = [
     {roll: 12, modifier: 2, type: "+2 Melee Weapon", other: "none"},
     {roll: 13, modifier: 2, type: "+2 Armor", other: "none"},
     {roll: 14, modifier: 1, type: "+1 Melee Weapon with Minor Ability", other: "Minor Ability"},
-    {roll: 15, modifier: 3, type: "+3 Missile Weapon)s)", other: "none"},
+    {roll: 15, modifier: 3, type: "+3 Missile Weapon(s)", other: "none"},
     {roll: 16, modifier: 3, type: "+3 Melee Weapon", other: "none"},
     {roll: 17, modifier: 3, type: "+3 Shield", other: "none"},
     {roll: 18, modifier: 3, type: "+3 Armor", other: "none"},
@@ -21,7 +21,7 @@ const magicalWeaponsAndArmor = [
     {roll: 20, modifier: 0, type: "Unusual Armor (optional) or Re-roll", other: "none"}
 ];
 
-const cursedArmorWeaponsSheilds = [
+const cursedArmorWeaponsShields = [
     {roll: 1, modifier: -1, type: "-1 Weapon or Armor"},
     {roll: 2, modifier: -1, type: "-1 Weapon or Armor"},
     {roll: 3, modifier: -1, type: "-1 Weapon or Armor"},
@@ -30,7 +30,7 @@ const cursedArmorWeaponsSheilds = [
     {roll: 6, modifier: 0, type: "Attracts missiles - Grants a +1 to-hit bonus to attack on such missiles."}
 ];
 
-const MinorAbilitiesForMeleeWeapons = [
+const minorAbilitiesForMeleeWeapons = [
     {roll: 1, type: "Additional damage (+1)"},
     {roll: 2, type: "Additional damage (+1)"},
     {roll: 3, type: "Additional damage (+1)"},
@@ -76,3 +76,30 @@ const magicMissileWeapons = [
     {roll: 19, type: "2d6 Bolts, crossbow"},
     {roll: 20, type: "2d6 Bolts, crossbow"},
 ];
+
+const magicUnusualWeapons = [
+    {roll: 1, type: "+1 Blunt weapon that destroys undead", description: "Blunt weapons are the mace, sling, staff, and war hammer. Lesser undead types (fewer than 4 HD) don't get a saving throw; more powerful ones do."},
+    {roll: 2, type: "+1 Thrown weapon that returns to hand", description: "These weapons are the hand axe, spear, and war hammer."},
+    {roll: 3, type: "+1 Weapon, grants 1 additional attack/day", description: "None"},
+    {roll: 4, type: "+1, +2 vs. particular type of foe", description: "Examples include dragons, giants, orcs, lycanthropes, undead, etc."},
+    {roll: 5, type: "+1, +4 vs. particular type of foe", description: "Examples include dragons, giants, orcs, lycanthropes, undead, etc."},
+    {roll: 6, type: "+2, +3 vs. particular type of foe", description: "Examples include dragons, giants, orcs, lycanthropes, undead, etc."},
+    {roll: 7, type: "Weapon Flames", description: "Additional 1d6 fire damage: roll 1d4-1 for “to-hit” bonus."},
+    {roll: 8, type: "Weapon Freezes", description: "Additional 1d6 cold damage: roll 1d4-1 for “to-hit” bonus."},
+    {roll: 9, type: "Dancing Weapon", description: "Fights in the air after 3 rounds, as a +1d3 weapon."},
+    {roll: 10, type: "Intelligent Weapon", description: "Roll 1d3 for to-hit bonus. There is a 10% chance that such a weapon may have the ability to cast a spell once per day. Such swords generally can communicate with their bearers, and often (25% chance) can speak audibly."},
+];
+
+const magicUnusualArmor = [
+    {roll: 1, type: "Armor of Arrow Deflection", description: "+2 against missile fire."},
+    {roll: 2, type: "Demonic Armor", description: "Possessed by a spirit or demon, with effects to be determined by the Referee."},
+    {roll: 3, type: "Ethereal Armor", description: "+3 plate mail that also allows the wearer to become insubstantial and incorporeal 50 times, after which it reverts to normal +3 platemail. In ethereal form, the wearer cannot be hit and cannot attack (unless the opponent is also ethereal)."},
+    {roll: 4, type: "Fiery Armor", description: "+1 armor that is surrounded by flames. These flames deal 1d6 damage to melee attackers."},
+];
+
+const miscItems = [
+    {start: 1, end: 1, type: "Lesser Wand"},
+    {start: 2, end: 2, type: "Lesser Ring"},
+    {start: 3, end: 20, type: "Misc. Lesser Magical Item"},
+    // to be continued...
+]
